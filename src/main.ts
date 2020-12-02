eecd7832184d2ee995853f39da08201663c5195b
jamborghini <atheistbut@muslim.com>
2020-12-02 19:10:23 +0300

    add source-map-support

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import 'source-map-support/register';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({transform: true}));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
