eeca1817c1f0432a17ba2725be8da1a6f3aa9cf3
jamborghini <atheistbut@muslim.com>
2021-02-06 00:39:50 +0300

    add insertion prevent method

--- src/suggested/entity/suggested.ts
+++ src/suggested/entity/suggested.ts
@@ -1,15 +1,29 @@
 import { CustomEntityHandler } from 'src/shared/util/CustomEntityHandler';
-import { Column, JoinColumn, ManyToOne } from 'typeorm';
+import { AfterLoad, BeforeInsert, Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { User } from 'src/user/entity/user';
+import { UnprocessableEntityException } from '@nestjs/common';
 
+@Entity()
 export class Suggested extends CustomEntityHandler {
 
-  @Column()
+  @Column({ unique: true })
   link: string;
 
   @ManyToOne(() => User, suggester => suggester.suggestions)
   @JoinColumn()
   suggester: User;
 
+  @BeforeInsert()
+  private async preventInsertion() {
+    const suggests = await Suggested.count();
+    if (suggests > 4) {
+      throw new UnprocessableEntityException('Maximum suggestions exceeded');
+    }
+  }
+
+  @AfterLoad()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private async deleteInsertion() {
+  }
 
 }
