53034345d0947834712f2948e4696d62bd152f20
jamborghini <atheistbut@muslim.com>
2021-02-06 00:39:24 +0300

    import user module

--- src/suggested/suggested.module.ts
+++ src/suggested/suggested.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SuggestedController } from './suggested.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Suggested } from 'src/suggested/entity/suggested';
+import { UserModule } from 'src/user/user.module';
+import { User } from 'src/user/entity/user';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Suggested])],
+  imports: [
+    UserModule,
+    TypeOrmModule.forFeature([Suggested])
+  ],
   controllers: [SuggestedController],
 })
 export class SuggestedModule {
