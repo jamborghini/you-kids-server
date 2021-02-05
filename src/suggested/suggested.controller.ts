24af908860b8312064909632ac36cdb8a23a52ca
jamborghini <atheistbut@muslim.com>
2021-02-06 00:39:12 +0300

    remove find suggested by ids since there is no point :')

--- src/suggested/suggested.controller.ts
+++ src/suggested/suggested.controller.ts
@@ -5,7 +5,6 @@ import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { MinRole } from 'src/auth/decorator/roles.';
 import { Role } from 'src/user/enum/role';
-import { Id } from 'src/shared/decorator/id';
 
 @Controller('suggested')
 export class SuggestedController {
@@ -13,19 +12,15 @@ export class SuggestedController {
   @MinRole(Role.User)
   @UseGuards(JwtGuard, RolesGuard)
   @Post()
-  async createBook(@Body() body: CreateSuggested, @Req() req): Promise<Suggested> {
+  async createSuggestion(@Body() body: CreateSuggested, @Req() req): Promise<Suggested> {
     console.log(req);
     body.suggester = req.user;
     return Suggested.create(body).save();
   }
 
-  @Get(':id')
-  async getSuggested(@Id() id: number): Promise<Suggested> {
-    return Suggested.findOne({ id });
-  }
 
   @Get()
   async getSuggesteds(): Promise<Suggested[]> {
-    return Suggested.find({ relations: ['suggester'] });
+    return Suggested.find();
   }
 }
